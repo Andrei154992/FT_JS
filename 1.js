b631618f898297b03184fc76d3916c1f169c931b
Andrei <andrei21.klei@gmail.com>
2025-03-01 02:32:33 +0400

    lab6

--- 1.js
+++ 1.js
@@ -1,29 +1,68 @@
-function array_1(array){
-    let sum = 0;
-    let count = 0;
-    for(let i = 0; i < array.length; i++){
-        sum += array[i]
-        if (array[i] == 0){
-            sum = 0
-            count++;
+function Task1(arr1, arr2) {
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+
+    const elements = [...set1].filter(element => set2.has(element));
+
+    return elements;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+console.log(Task1(array1, array2)); 
+
+function Task2(arr) {
+    const map = new Map();
+    
+    for (const element of arr) {
+        if (map.has(element)) {
+            map.set(element, map.get(element) + 1);
+        } else {
+            map.set(element, 1);
         }
     }
-    if (count == 0) sum = 0
-    return sum;
+
+    return map;
 }
 
-function array_2(matrix){
-    let sum = 0;
-    for (let row of matrix){
-        if (row.some(element => element < 0))
-            sum += row.reduce((acc, val) => acc + val, 0);        
+const array = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+console.log(Task2(array)); 
+
+class Student {
+    constructor(name, group) {
+        this.name = name;
+        this.group = group;
+    }
+}
+
+function Task3(students) {
+    const groupCounts = new Map();
+
+    for (const student of students) {
+        if (groupCounts.has(student.group)) {
+            groupCounts.set(student.group, groupCounts.get(student.group) + 1);
+        } else {
+            groupCounts.set(student.group, 1);
+        }
     }
     
-    return sum;
+    let maxCount = 0;
+    for (const count of groupCounts.values()) {
+        if (count > maxCount) {
+            maxCount = count;
+        }
+    }
+
+    return maxCount;
 }
 
-let array = [10, 6, 0, 3, 7, 0, 8, 44, 9]
-let matrix = [[10, 11,0], [3, -7, 98], [99, 3, 5]]
+const students = [
+    new Student("Андрей", "6203"),
+    new Student("Артём", "5134"),
+    new Student("Павел", "5134"),
+    new Student("Никита", "6203"),
+    new Student("Виктор", "6203"),
+    new Student("Руслан", "3456")
+];
 
-alert(`1 задание: ${array_1(array)}`)
-alert(`2 задание: ${array_2(matrix)}`)
+console.log(Task3(students));
